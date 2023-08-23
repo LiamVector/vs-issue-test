9ed4aedb776e3e36b5683f4725a19710cc99232e
Liam Routt <liam@vectorstorm.com.au>
2023-08-23 18:48:47 +1000

    Create empty tables

--- migrations/20230823081541_initial.sql
+++ migrations/20230823081541_initial.sql
@@ -1,1 +1,56 @@
--- Add migration script here
+CREATE TABLE IF NOT EXISTS info (
+    infoid INTEGER PRIMARY KEY,
+    dataversion INTEGER NOT NULL,
+    scanversion INTEGER NOT NULL,
+    scandate DATE
+);
+CREATE TABLE IF NOT EXISTS crashes (
+    reportid INTEGER PRIMARY KEY,
+    userid INTEGER,
+    platform TEXT,
+    version TEXT,
+    vernum INTEGER,
+    mmoname TEXT,
+    reportdate TIMESTAMPTZ NOT NULL,
+    reporttime TEXT,
+    aser TEXT,
+    cond TEXT,
+    location TEXT,
+    cpu TEXT,
+    ram TEXT,
+    gpu TEXT,
+    gpuver TEXT,
+    diskavail INTEGER,
+    modded BOOL,
+    recognized TEXT,
+    stack TEXT,
+    loglines TEXT
+);
+CREATE INDEX ver ON crashes(vernum);
+CREATE INDEX usr ON crashes(userid);
+CREATE INDEX date ON crashes(reportdate);
+CREATE TABLE IF NOT EXISTS users (
+    userid INTEGER PRIMARY KEY,
+    steamid INTEGER NOT NULL UNIQUE
+);
+CREATE UNIQUE INDEX steam ON users(steamid);
+CREATE TABLE IF NOT EXISTS functions (
+    functid INTEGER PRIMARY KEY,
+    function TEXT NOT NULL UNIQUE
+);
+CREATE TABLE IF NOT EXISTS conditions (
+    condid INTEGER PRIMARY KEY,
+    cond TEXT NOT NULL,
+    location TEXT NOT NULL
+);
+CREATE TABLE IF NOT EXISTS issues (
+    issueid INTEGER PRIMARY KEY,
+    stack1 INTEGER NOT NULL,
+    stack2 INTEGER NOT NULL,
+    stack3 INTEGER NOT NULL,
+    condition INTEGER,
+    FOREIGN KEY(stack1) REFERENCES functions(functid),
+    FOREIGN KEY(stack2) REFERENCES functions(functid),
+    FOREIGN KEY(stack3) REFERENCES functions(functid),
+    FOREIGN KEY(condition) REFERENCES conditions(condid)
+);
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,18 @@
 use axum::{routing::get, Router};
+use sqlx::PgPool;
 
 async fn hello_world() -> &'static str {
     "Hello, world!"
 }
 
 #[shuttle_runtime::main]
-async fn axum() -> shuttle_axum::ShuttleAxum {
+async fn axum (
+    #[shuttle_shared_db::Postgres] pool: PgPool
+) -> shuttle_axum::ShuttleAxum {
+    sqlx::migrate!().run(&pool)
+        .await
+        .expect("Migrations failed :(");
+
     let router = Router::new().route("/", get(hello_world));
 
     Ok(router.into())
